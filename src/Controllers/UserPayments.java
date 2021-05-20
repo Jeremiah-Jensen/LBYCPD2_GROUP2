4bb597fcec8d6d117fbb45770660584ea6ae7cb5
Antonb28 <62889116+Antonb28@users.noreply.github.com>
2021-05-20 13:13:58 +0800

    User Payments Improved

--- src/Controllers/UserPayments.java
+++ src/Controllers/UserPayments.java
@@ -1,7 +1,11 @@
 package Controllers;
 
+import Models.Appointments;
 import Models.User;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -20,40 +24,64 @@ import java.util.ResourceBundle;
 public class UserPayments implements Initializable {
     @FXML
     public Button LogOutButton, AppointmentsButton, DetailsButton, HomeButton, LoadCreditButton;
-    public TextField NameTextField, CardNumTextField, ExpirydateTextField, BankTextField, NetworkTextField;
+    public TextField NameTextField, CardNumTextField;
     public PasswordField CVVPasswordField;
-    public AnchorPane InputCard, CardDetails;
-    public Label NameLabel, CardNumLabel, ExpirydateLabel, CVVLabel, BankLabel, NetworkLabel;
-
+    public ListView<String> TransactionHistory;
+    public AnchorPane InputCard, CardDetails, History;
+    public Label NameLabel, CardNumLabel, ExpirydateLabel, NetworkLabel, CVVLabel;
+    public Label NameLabel2, CardNumLabel2, ExpirydateLabel2, NetworkLabel2;
+    public ComboBox<String> Month, Year, Network;
+    Firebase firebase = new Firebase("https://lbycpd2-grp2-default-rtdb.firebaseio.com");
+    List<Appointments> appointmentsList = new ArrayList<>();
     User userModel;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         userModel = UserLogIn.userModel;
-        NameTextField.setText(userModel.getName());
-        CardNumTextField.setText(userModel.getCardnumber());
-        ExpirydateTextField.setText(userModel.getExpirydate());
-        CVVPasswordField.setText(userModel.getCvv());
-        BankTextField.setText(userModel.getBank());
-        NetworkTextField.setText(userModel.getNetwork());
+        NameLabel2.setText(userModel.getName());
+        CardNumLabel2.setText(userModel.getCardnumber());
+        ExpirydateLabel2.setText(userModel.getExpirydate());
+        NetworkLabel2.setText(userModel.getNetwork());
+
+        Month.getItems().addAll("01","02","03","04","05","06","07","08","09","10","11","12");
+        Year.getItems().addAll("21","22","23","24","25","26","27","28","29","30","31","32");
+        Network.getItems().addAll("Visa", "Mastercard","JCB");
+        firebase.child("Appointments").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                    Appointments app = data.getValue(Appointments.class);
+                    appointmentsList.add(app);
+                }
+                System.out.println(appointmentsList.size());
+                for(int i = 0; i < appointmentsList.size(); i++) {
+                    Appointments appointmentsModel = appointmentsList.get(i);
+                    String FullName = userModel.getFirstName() + " " + userModel.getLastName();
+                    if(FullName.equals(appointmentsModel.getUser())){
+                        if(appointmentsModel.getStatus().equals("Upcoming") || appointmentsModel.getStatus().equals("Consultation")){
+
+                        }
+                        else{
+                            TransactionHistory.getItems().add("500 PHP - " + appointmentsModel.getAppointment());
+                        }
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
     }
 
     public void Switch(ActionEvent actionEvent) {
-        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(1), InputCard);
-        translateTransition.setByY(-321);
-        translateTransition.play();
-        TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(1), CardDetails);
-        translateTransition1.setByY(321);
-        translateTransition1.play();
+        InputCard.setVisible(true);
+        History.setVisible(false);
     }
 
     public void GoBack(ActionEvent actionEvent) {
-        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(1), InputCard);
-        translateTransition.setByY(321);
-        translateTransition.play();
-        TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(1), CardDetails);
-        translateTransition1.setByY(-321);
-        translateTransition1.play();
+        InputCard.setVisible(false);
+        History.setVisible(true);
     }
 
 
@@ -89,7 +117,7 @@ public class UserPayments implements Initializable {
         alert.setContentText("Exit this dialog box to continue editing\nClick ok to confirm");
         Optional<ButtonType> result = alert.showAndWait();
 
-        if(CardNumTextField.getText().length() == 16 && CVVPasswordField.getText().length() == 3 && ExpirydateTextField.getText().length() == 5){
+        if(CardNumTextField.getText().length() == 16 && CVVPasswordField.getText().length() == 3 && !Month.getValue().isEmpty() && !Year.getValue().isEmpty() && !Network.getValue().isEmpty()){
             if(result.isPresent()&&result.get()==ButtonType.OK){
                 if(userModel==null){
                     throw new NullPointerException("Can't pass null for argument 'pathString' in child()");
@@ -98,17 +126,18 @@ public class UserPayments implements Initializable {
                     firebase.child("User").child(userModel.getId()).child("name").setValue(NameTextField.getText());
                     firebase.child("User").child(userModel.getId()).child("cardnumber").setValue(CardNumTextField.getText());
                     firebase.child("User").child(userModel.getId()).child("cvv").setValue(CVVPasswordField.getText());
-                    firebase.child("User").child(userModel.getId()).child("expirydate").setValue(ExpirydateTextField.getText());
-                    firebase.child("User").child(userModel.getId()).child("bank").setValue(BankTextField.getText());
-                    firebase.child("User").child(userModel.getId()).child("network").setValue(NetworkTextField.getText());
+                    firebase.child("User").child(userModel.getId()).child("expirydate").setValue(Month.getValue()+"/"+Year.getValue());
+                    firebase.child("User").child(userModel.getId()).child("network").setValue(Network.getValue());
                     NameTextField.clear();
                     CardNumTextField.clear();
                     CVVPasswordField.clear();
-                    ExpirydateTextField.clear();
-                    BankTextField.clear();
-                    NetworkTextField.clear();
-                    LoadCreditButton.setDisable(false);
-
+                    InputCard.setVisible(false);
+                    History.setVisible(true);
+                    UserLogIn.userModel = userModel;
+                    NameLabel2.setText(userModel.getName());
+                    CardNumLabel2.setText(userModel.getCardnumber());
+                    ExpirydateLabel2.setText(userModel.getExpirydate());
+                    NetworkLabel2.setText(userModel.getNetwork());
                 }
 
             }
@@ -135,15 +164,11 @@ public class UserPayments implements Initializable {
             if (CVVPasswordField.getText().length() != 3 || CVVPasswordField.getText().isEmpty()) {
                 CVVLabel.setText("Must be 3 digits");
             }
-            if (ExpirydateTextField.getText().length() != 5 || ExpirydateTextField.getText().isEmpty()) {
-                ExpirydateLabel.setText("Must be 5 digits");
+            if (Month.getValue().isEmpty() || Year.getValue().isEmpty() || (Month.getValue().isEmpty() && Year.getValue().isEmpty())) {
+                ExpirydateLabel.setText("Enter Expiry Date");
             }
 
-            if (BankTextField.getText().isEmpty()) {
-                BankLabel.setText("Empty field");
-            }
-
-            if (NameTextField.getText().isEmpty()) {
+            if (Network.getValue().isEmpty()) {
                 NetworkLabel.setText("Empty field");
             }
 
